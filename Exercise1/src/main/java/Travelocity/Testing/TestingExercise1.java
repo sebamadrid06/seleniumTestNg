c80f27823cbd133af99132f32b45819b3d598f79
Sebastian <hugo.madrid>
2018-07-06 17:24:00 -0300

    Stale Element issue resolved

--- Exercise1/src/main/java/Travelocity/Testing/TestingExercise1.java
+++ Exercise1/src/main/java/Travelocity/Testing/TestingExercise1.java
@@ -2,18 +2,35 @@ package Travelocity.Testing;
 
 import Travelocity.TestsLayer.HomePageTests;
 import Travelocity.TestsLayer.ResultsPageTests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class TestingExercise1 extends TestBase {
 
 @Test
-public void init(){
+public void init() throws InterruptedException {
 
     HomePageTests home = getHomePage();
     ResultsPageTests results = home.searchFlightAction();
     results.checkButton();
-    results.checkElements();
+
+
     results.checkFlightDurationElement();
+
+    for (int i=0;i<4;i++)
+
+        try{
+            results.checkElements();
+            break;
+        }catch(StaleElementReferenceException e){
+        e.toString();
+            System.out.println("Trying to recover from Stale element...:  ");
+        }
+
 }
 
 }
--- Exercise1/src/main/java/Travelocity/TestsLayer/ResultsPageTests.java
+++ Exercise1/src/main/java/Travelocity/TestsLayer/ResultsPageTests.java
@@ -2,8 +2,10 @@ package Travelocity.TestsLayer;
 
 import Travelocity.PagesLayer.ResultsPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
@@ -15,8 +17,7 @@ public class ResultsPageTests extends ResultsPage {
     public void checkButton() {
         List<WebElement> obj = getDriver().findElements(By.cssSelector("#flightModuleList li:nth-child(N) button"));
 
-        for (int i = 0 ; i <obj.size() ; i++)
-        {
+        for (int i = 0; i < obj.size(); i++) {
             WebElement item = obj.get(i);
             System.out.println(item.getText());
 
@@ -26,11 +27,15 @@ public class ResultsPageTests extends ResultsPage {
 
 
     public void checkFlightDurationElement() {
+        getWait().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#flightModuleList li:nth-child(N) span[data-test-id='duration']")));
+
 
         List<WebElement> flightDurationElement = getDriver().findElements(By.cssSelector("#flightModuleList li:nth-child(N) span[data-test-id='duration']"));
 
         for (int i = 0; i < flightDurationElement.size(); i++) {
+
             WebElement item = flightDurationElement.get(i);
+
             System.out.println(item.getText());
 
         }
