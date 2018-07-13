e1db78e4aa38f94007c4d71dc9a2d31d14fce93c
Sebastian <hugo.madrid>
2018-07-13 18:04:23 -0300

    Done with R8 - Verify validations - Exercise 1 finished

--- Exercise1/src/main/java/Travelocity/PagesLayer/PaymentPage.java
+++ Exercise1/src/main/java/Travelocity/PagesLayer/PaymentPage.java
@@ -11,10 +11,37 @@ public class PaymentPage extends BasePage {
     }
 
 
-    @FindBy(css = "span[class='icon icon-close']")
-    private WebElement closeButton;
-    public WebElement getCloseButton() {
-        return closeButton;
+    @FindBy(css = "h2[class='faceoff-module-title']")
+    private WebElement sectionTitle;
+    @FindBy(id = "firstname[0]")
+    private WebElement firstNameField;
+    @FindBy(id = "middlename[0]")
+    private WebElement middleName;
+    @FindBy(id = "lastname[0]")
+    private WebElement lastName;
+    @FindBy(id = "payments")
+    private WebElement payment;
+
+
+    public WebElement getSectionTitle() {
+        return sectionTitle;
     }
 
+    public WebElement getFirstNameField() {
+        return firstNameField;
+    }
+
+    public WebElement getMiddleName() {
+        return middleName;
+    }
+
+    public WebElement getLastName() {
+        return lastName;
+    }
+
+    public WebElement getPayment() {
+        return payment;
+    }
 }
+
+
--- Exercise1/src/main/java/Travelocity/TestsLayer/PaymentPageTests.java
+++ Exercise1/src/main/java/Travelocity/TestsLayer/PaymentPageTests.java
@@ -1,26 +1,47 @@
 package Travelocity.TestsLayer;
 
 import Travelocity.PagesLayer.PaymentPage;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 public class PaymentPageTests extends PaymentPage {
     public PaymentPageTests(WebDriver pdriver) {
         super(pdriver);
     }
 
-    /*public void verifyValidations() {
+    public void verifyValidations() {
         boolean alertPresent = false;
         try {
 
             Alert alert = getDriver().switchTo().alert();
             alertPresent = true;
             alert.dismiss();
-            getWait().until(ExpectedConditions.elementToBeClickable(getBookButton()));
-            getBookButton().click();
+            getWait().until(ExpectedConditions.elementToBeClickable(getSectionTitle()));
+            Assert.assertTrue(getSectionTitle().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getFirstNameField()));
+            Assert.assertTrue(getFirstNameField().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getMiddleName()));
+            Assert.assertTrue(getMiddleName().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getLastName()));
+            Assert.assertTrue(getLastName().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getPayment()));
+            Assert.assertTrue(getPayment().isDisplayed());
+
         } catch (NoAlertPresentException e) {
-            getWait().until(ExpectedConditions.elementToBeClickable(getBookButton()));
-            getBookButton().click();
+            getWait().until(ExpectedConditions.elementToBeClickable(getSectionTitle()));
+            Assert.assertTrue(getSectionTitle().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getFirstNameField()));
+            Assert.assertTrue(getFirstNameField().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getMiddleName()));
+            Assert.assertTrue(getMiddleName().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getLastName()));
+            Assert.assertTrue(getLastName().isDisplayed());
+            getWait().until(ExpectedConditions.elementToBeClickable(getPayment()));
+            Assert.assertTrue(getPayment().isDisplayed());
         }
 
-    }*/
+    }
 }
